ddd9d0fb1fe6aa944e7d41c6ad4d02bf480377f5
cloveridge <christian.loveridge@gmail.com>
2025-03-19 14:59:59 -0600

    Web Scraping Start

---
+++ WebScrapingForIngestion/UVUJobScraper.py
@@ -0,0 +1,22 @@
+# class JobListing:
+#     def __init__(self,site,title,salary,postdate,description):
+#         self.site = site
+#         self.title = title
+#         self.salary = salary
+#         self.postdate = postdate
+#         self.description = description
+
+def ScrapeJobs_UVU(site,search):
+    pass
+
+if __name__ == '__main__':
+    pass
+    searches = ['data engineer','data warehouse','data']
+    sites_to_search = [
+        {'Company':'UVU','Website':'https://www.schooljobs.com/careers/uvu'}
+    ]
+    jobs_list = []
+
+    for site in sites_to_search:
+        for search in searches:
+            ScrapeJobs_UVU(site,search)
