4a58a538fa6f5e8e45ded1802fdead437b07077d
cloveridge <christian.loveridge@gmail.com>
2025-03-25 20:37:28 -0600

    Webscraping first steps

--- WebScrapingForIngestion/UVUJobScraper.py
+++ WebScrapingForIngestion/UVUJobScraper.py
@@ -1,24 +1,34 @@
 # import beautifulsoup as bs
+import selenium
+from selenium.webdriver import Edge,EdgeOptions,Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-# class JobListing:
-#     def __init__(self,site,title,salary,postdate,description):
-#         self.site = site
-#         self.title = title
-#         self.salary = salary
-#         self.postdate = postdate
-#         self.description = description
+from bs4 import BeautifulSoup as bs
+
+
 
 def ScrapeJobs_UVU(site,search):
-    pass
+    search_url = f'{site['Website']}?&keywords={search.replace(' ','%20')}'
+    print(search_url)
+    
+    driver = Edge
+    driver_options = EdgeOptions()
+    driver_options.add_argument("--headless")  # Run in headless mode (no UI)
+    driver_options.add_argument("--disable-blink-features=AutomationControlled")  # Avoid detection
+    
+    driver = driver()
+
+
 
 if __name__ == '__main__':
-    pass
     searches = ['data engineer','data warehouse','data']
     sites_to_search = [
-        {'Company':'UVU','Website':'https://www.schooljobs.com/careers/uvu'}
+        {'Company':'UVU','Website':'https://www.schooljobs.com/careers/uvu','Function':ScrapeJobs_UVU}
     ]
     jobs_list = []
 
     for site in sites_to_search:
         for search in searches:
-            ScrapeJobs_UVU(site,search)
+            site['Function'](site,search)
